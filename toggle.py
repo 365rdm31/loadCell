a7a533d22674655ad4b171ea98d2b6bfda347156
365rdm31 <rydmers@gmail.com>
2020-04-14 22:20:45 -0500

    Create toggle.py

---
+++ toggle.py
@@ -0,0 +1,9 @@
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+#def_
